ed4d52268960b2f8c6c16244d741a8f16a1b727f
Bryan Berger <bryan@bryanberger.com>
2022-02-20 20:47:05 -0500

    init delete stuff and update pkgs

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,10 +43,24 @@ module.exports = {
       resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
+          // {
+          //   resolve: "gatsby-remark-relative-images",
+          //   options: {
+          //     name: "uploads",
+          //   },
+          // },
           {
-            resolve: "gatsby-remark-relative-images",
+            resolve: `gatsby-remark-relative-images`,
             options: {
-              name: "uploads",
+              // [Optional] The root of "media_folder" in your config.yml
+              // Defaults to "static"
+              // staticFolderName: 'static',
+              // [Optional] Include the following fields, use dot notation for nested fields
+              // All fields are included by default
+              // include: ['featured'],
+              // [Optional] Exclude the following fields, use dot notation for nested fields
+              // No fields are excluded by default
+              // exclude: ['featured.skip'],
             },
           },
           {
